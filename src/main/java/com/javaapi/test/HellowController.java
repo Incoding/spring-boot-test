a82d3bda80ec6617a8b6daa7458053b558d4242d
wangkun <wangkun@acfun.tv>
2016-06-05 11:41:41 +0800

    spring boot sync

--- src/main/java/com/javaapi/test/HellowController.java
+++ src/main/java/com/javaapi/test/HellowController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+//You should only ever add one @EnableAutoConfiguration annotation. We generally recommend that you add it to your primary @Configuration class.
 @Controller
 @EnableAutoConfiguration
 @ImportResource("classpath:application-context.xml")
