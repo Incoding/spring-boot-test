51a95a708c80a6db958b964d8d62f84384d4bc75
wangkun <wangkun@acfun.tv>
2016-06-05 19:22:27 +0800

    spring boot   test getRealPath

--- src/main/java/com/javaapi/test/HellowController.java
+++ src/main/java/com/javaapi/test/HellowController.java
@@ -14,6 +14,8 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
+
 //You should only ever add one @EnableAutoConfiguration annotation. We generally recommend that you add it to your primary @Configuration class.
 @EnableTransactionManagement
 @Controller
@@ -30,14 +32,10 @@ public class HellowController {
     @Autowired
     private UserService userService;
 
-    @Bean
-    public Object testBean(PlatformTransactionManager platformTransactionManager){
-        System.out.println(">>>>>>>>>>" + platformTransactionManager.getClass().getName());
-        return new Object();
-    }
-
-
-
+    /**
+     * 测试controller
+     * @return
+     */
     @RequestMapping("/hellow")
     @ResponseBody
     public String hellow(){
@@ -45,6 +43,12 @@ public class HellowController {
         return "哈喽，Spring Boot ！";
     }
 
+
+    /**
+     * 测试dao
+     * @param name
+     * @return
+     */
     @RequestMapping("/nihao")
          @ResponseBody
          public String nihao(String name) {
@@ -52,14 +56,55 @@ public class HellowController {
         return byUsername== null ? "user is not found":byUsername.toString();
     }
 
-
-
+    /**
+     * 测试事物
+     * @param id
+     * @return
+     */
     @RequestMapping("/updateUser")
     @ResponseBody
     public String updateUser(String id) {
         userService.updateUser(id);
         return "success";
     }
+
+
+    /**
+     * @param platformTransactionManager
+     * 测试事物的实现类
+     * @return
+     */
+    @Bean
+    public Object testBean(PlatformTransactionManager platformTransactionManager){
+        System.out.println(">>>>>>>>>>" + platformTransactionManager.getClass().getName());
+        return new Object();
+    }
+
+
+    /**
+     * 测试事物
+     * @param id
+     * @return
+     */
+    @RequestMapping("/getRealContext")
+    @ResponseBody
+    public String getRealContext(HttpServletRequest request) {
+        String realPath = request.getServletContext().getRealPath("/");
+        /**
+         * 没打包的情况下输出
+         /Users/user/workspace-2015-08-13/spring-boot-test/src/main/webapp/
+         *
+         */
+        System.out.println("realPath = " + realPath);
+        /** 没打包的情况下输出
+         /Users/user/workspace-2015-08-13/spring-boot-test
+         *
+         */
+        System.out.println("realPath = " + System.getProperty("user.dir"));
+
+        return "success";
+    }
+
 
 
     public static void main(String[] args) {
