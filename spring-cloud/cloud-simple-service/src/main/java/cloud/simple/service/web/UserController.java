c08ce0128cb2fad84ead5c6e4f6ef10a978c12e2
wangkun <wangkun@acfun.tv>
2016-07-30 11:00:08 +0800

    spring cloud

--- spring-cloud/cloud-simple-service/src/main/java/cloud/simple/service/web/UserController.java
+++ spring-cloud/cloud-simple-service/src/main/java/cloud/simple/service/web/UserController.java
@@ -1,13 +1,13 @@
 package cloud.simple.service.web;
 
-import java.util.List;
+import cloud.simple.service.domain.UserService;
+import cloud.simple.service.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import cloud.simple.service.domain.UserService;
-import cloud.simple.service.model.User;
+import java.util.List;
 
 @RestController
 public class UserController {
@@ -17,7 +17,8 @@ public class UserController {
 	
 	@RequestMapping(value="/user",method=RequestMethod.GET)
 	public List<User> readUserInfo(){
-		List<User> ls=userService.searchAll();		
-		return ls;
+		List<User> ls=userService.searchAll();
+        System.out.println("invoke");
+        return ls;
 	}
 }
